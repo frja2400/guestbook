cde5e95feb5ac62f4cabe77c0446e1521c50a894
Frida Jansson <frja2400@student.miun.se>
2025-04-10 18:18:04 +0200

    Version 2.0

--- server.js
+++ server.js
@@ -2,17 +2,21 @@
  * Gästbok
  */
 
+
 //Importerar alla moduler jag behöver
 const express = require("express");
 const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
+//Ladda miljövariabler från .env
+require('dotenv').config();
 
 //Skapar en anslutning till databasen
-const db = new sqlite3.Database("./db/guestbook.db");
+const db = new sqlite3.Database(process.env.DATABASE_URL); // Använd miljövariabel för databas-URL
+
 
 //Inställningar
 const app = express();                                          //Skapar en ny express-applikation
-const port = 3000;                                              //Ställer in porten till 3000
+const port = process.env.PORT || 3000;                          // Använd miljövariabel för port
 app.set("view engine", "ejs");                                  //Anger ejs som templating engine
 app.use(express.static("public"));                              //Konfigurerar Express att använda statiska filer från public-mappen.
 app.use(bodyParser.urlencoded({ extended: true }));             //Använder body-parser för att hantera URL-encoded data.
